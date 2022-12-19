441eb171c530646db9a1d86162a156a6e2e93620
Mateusz <czajkamateusz999@gmail.com>
2022-12-19 01:15:46 +0100

    add 5 letter words, read words from file

---
+++ WordleBackend/Wordle/Controllers/WordlesController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Wordle.Controllers {
+    [Route("api/v1/[controller]")]
+    public class WordlesController : Controller {
+
+        [HttpGet]
+        public string[] GetWords() {
+            string fileName = "sgb-words.txt";
+            string path = Path.Combine(Environment.CurrentDirectory, @"Data\FiveLetterWords", fileName);
+            string[] words = System.IO.File.ReadAllText(path)
+                .Split(
+                    new[] {' ', '\n', '\r', ','},
+                    StringSplitOptions.RemoveEmptyEntries
+                );
+
+            return words;
+        }
+    }
+}
